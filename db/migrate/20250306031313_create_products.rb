028706cd39b0f52cf6c5a5f8b307f3b540a5d862
nathanielpierson <nathanielepierson@gmail.com>
2025-03-05 22:16:48 -0500

    add scaffold

--- db/migrate/20250306031313_create_products.rb
+++ db/migrate/20250306031313_create_products.rb
@@ -1,6 +1,10 @@
 class CreateProducts < ActiveRecord::Migration[7.2]
   def change
     create_table :products do |t|
+      t.string :name
+      t.string :price
+      t.text :description
+
       t.timestamps
     end
   end
